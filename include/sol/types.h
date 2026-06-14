8ae4b94fa86a85bdc0c3fc06a3c4239bb8103e3d
Nykenik24 <Nykenik24@proton.me>
2026-06-14 21:12:39 +0200

    feat: some more types

--- include/sol/types.h
+++ include/sol/types.h
@@ -10,6 +10,10 @@ typedef unsigned long uint64;
 typedef unsigned short uint16;
 typedef unsigned char uint8;
 
+typedef long int64;
+typedef short int16;
+typedef char int8;
+
 typedef unsigned long size;
 
 #endif // !SOL_INCLUDE__TYPES_H
